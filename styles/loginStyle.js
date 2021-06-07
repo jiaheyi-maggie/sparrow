e4e95b7587ed1c7940bcf9a6dc82d68af0bfed9d
Maggie Yi <maggieyi@berkeley.edu>
2021-06-07 15:35:52 -0400

    half of onboarding UI 06/07/21

--- styles/loginStyle.js
+++ styles/loginStyle.js
@@ -7,4 +7,13 @@ const loginStyle = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    title: {
+      color: '#264653',
+      fontWeight: 'bold',
+      fontSize: 40,
+      textAlign: 'left',
+      margin: 20
+    }
   });
+
+  export default loginStyle;
