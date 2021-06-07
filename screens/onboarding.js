a96246db2e4547e805fb77de5baedac01ec347f6
Maggie Yi <maggieyi@berkeley.edu>
2021-06-07 14:08:42 -0400

    separate styles

--- screens/onboarding.js
+++ screens/onboarding.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import { Text, Button, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
+import styles from '../styles/onboardingStyle';
 
 // Customize Button Styles (put in ../components later) -> doesn't work right now
 const backgroundColor = isLight => (isLight ? '#264653' : 'aliceblue');
@@ -14,27 +15,27 @@ const dummyPress = () => Alert.alert("Button pressed");
 // Done Button
   const DoneButton = () => (
       <TouchableOpacity
-        style={onboardingStyle.buttonContainer}
+        style={styles.buttonContainer}
       >
-          <Text style={onboardingStyle.buttonText}> Done </Text>
+          <Text style={styles.buttonText}> Done </Text>
       </TouchableOpacity>
   );
 
   // Continue Button
   const ContinueButton = () => (
     <TouchableOpacity
-      style={onboardingStyle.buttonContainer}
+      style={styles.buttonContainer}
     >
-        <Text style={onboardingStyle.buttonText}> Continue </Text>
+        <Text style={styles.buttonText}> Continue </Text>
     </TouchableOpacity>
 );
 
 // Skip Button
 const SkipButton = () => (
     <TouchableOpacity
-      style={onboardingStyle.buttonContainer}
+      style={styles.buttonContainer}
     >
-        <Text style={onboardingStyle.buttonText}> Skip </Text>
+        <Text style={styles.buttonText}> Skip </Text>
     </TouchableOpacity>
 );
 
@@ -53,8 +54,8 @@ const onboarding = ({navigation}) => {
                     resizeMode='contain'
                     style={{width: 300, height: 300}}
                 />,
-                title: <Text style={onboardingStyle.title}> Welcome </Text>,
-                subtitle: <Text style={onboardingStyle.subtitle}> Congrats on taking the fist step to managing your money efficiently! Let us get to know your spending habits with some basic questions :))</Text>,
+                title: <Text style={styles.title}> Welcome </Text>,
+                subtitle: <Text style={styles.subtitle}> Congrats on taking the fist step to managing your money efficiently! Let us get to know your spending habits with some basic questions :))</Text>,
             },
             {
                 backgroundColor: '#fff',
@@ -64,8 +65,8 @@ const onboarding = ({navigation}) => {
                     style={{width: 300, height: 300}}
                 />,
                 title: <Text adjustFontSizeToFit minimumFontScale={.5} 
-                        style={onboardingStyle.title}> Mark Categories </Text>,
-                subtitle: <Text style={onboardingStyle.subtitle}>This does not have to be perfect, just an estimate!</Text>,
+                        style={styles.title}> Mark Categories </Text>,
+                subtitle: <Text style={styles.subtitle}>This does not have to be perfect, just an estimate!</Text>,
             },
             {
                 backgroundColor: '#fff',
@@ -74,49 +75,49 @@ const onboarding = ({navigation}) => {
                     resizeMode='contain'
                     style={{width: 300, height: 300, alignContent: 'center'}}
                 />,
-                title: <Text style={onboardingStyle.title}> Do you have a long-term budget? </Text>,
-                subtitle: <Text style={onboardingStyle.subtitle}> This way we can start by suggesting a budget that works for you. </Text>,
+                title: <Text style={styles.title}> Do you have a long-term budget? </Text>,
+                subtitle: <Text style={styles.subtitle}> This way we can start by suggesting a budget that works for you. </Text>,
             },
         ]}
         />
     );
 };
 
-const onboardingStyle = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-        color: '#264653',
-        fontWeight: 'bold',
-        fontSize: 40,
-        textAlign: 'center'
-    },
-    subtitle: {
-        color: '#E76F51',
-        fontWeight: 'bold',
-        textAlign: 'center',
-        fontSize: 18,
-        margin: 15
-    },
-    buttonContainer: {
-        elevation: 8,
-        backgroundColor: "#264653",
-        borderRadius: 10,
-        paddingVertical: 9,
-        paddingHorizontal: 9,
-        margin: 10
-    },
-    buttonText : {
-        fontSize: 15,
-        color: "#fff",
-        alignSelf: "center",
-        textTransform: "uppercase"
-    }
-  });
+// const onboardingStyle = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//     title: {
+//         color: '#264653',
+//         fontWeight: 'bold',
+//         fontSize: 40,
+//         textAlign: 'center'
+//     },
+//     subtitle: {
+//         color: '#E76F51',
+//         fontWeight: 'bold',
+//         textAlign: 'center',
+//         fontSize: 18,
+//         margin: 15
+//     },
+//     buttonContainer: {
+//         elevation: 8,
+//         backgroundColor: "#264653",
+//         borderRadius: 10,
+//         paddingVertical: 9,
+//         paddingHorizontal: 9,
+//         margin: 10
+//     },
+//     buttonText : {
+//         fontSize: 15,
+//         color: "#fff",
+//         alignSelf: "center",
+//         textTransform: "uppercase"
+//     }
+//   });
 
 
 export default onboarding;
--- styles/onboardingStyle.js
+++ styles/onboardingStyle.js
@@ -1,12 +1,39 @@
 import { StyleSheet } from 'react-native';
 
 const onboardingStyle = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+      color: '#264653',
+      fontWeight: 'bold',
+      fontSize: 40,
+      textAlign: 'center'
+  },
+  subtitle: {
+      color: '#E76F51',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      fontSize: 18,
+      margin: 15
+  },
+  buttonContainer: {
+      elevation: 8,
+      backgroundColor: "#264653",
+      borderRadius: 10,
+      paddingVertical: 9,
+      paddingHorizontal: 9,
+      margin: 10
+  },
+  buttonText : {
+      fontSize: 15,
+      color: "#fff",
+      alignSelf: "center",
+      textTransform: "uppercase"
+  }
+});
 
 export default onboardingStyle;
