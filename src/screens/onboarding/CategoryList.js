461ff3d983a62358a8b389f335bf4e9e9713def0
Maggie Yi <maggieyi@berkeley.edu>
2021-06-11 11:35:32 -0400

    new set to prevent duplicate

--- src/screens/onboarding/CategoryList.js
+++ src/screens/onboarding/CategoryList.js
@@ -9,22 +9,21 @@ import selected from '../../data/selected';
 const CategoryList = () => {
 
     // get selected categories
-    const [checkedCategories, setCheckedCategories] = useState([]);
+    const [checkedCategories, setCheckedCategories] = useState(new Set());
 
-    // get booleans for checkboxes
+    // get booleans for checkboxes (an array initially)
     const [checked, setChecked] = useState(selected);
-
 
 
     // when user press on an item, add item to checkedCategories
     const pressHandler = (id) => {
-        // setCheckedCategories((prev) => {
-        //     // return prev.filter( category => category.id != id);
-        // })
         setChecked(selected[id].checked = !selected[id].checked);
         console.log(selected);
-        setCheckedCategories([...checkedCategories, categories[id]]);
-        // console.log(checkedCategories);
+        //[...checkedCategories, categories[id]]
+
+        // is this memory expensive? 
+        setCheckedCategories(prev => new Set([...checkedCategories, categories[id]]));
+        console.log(checkedCategories);
     };
 
     // iterate through checkedCategories and render 
@@ -32,7 +31,7 @@ const CategoryList = () => {
         <FlatList 
             data={categories}
             renderItem={({item}) => (<CategoryItem item={item} pressHandler={pressHandler} />)}
-            keyExtractor={item => item.title}
+            keyExtractor={item => item.id}
             contentContainerStyle={{
                 flexGrow: 1,
             }}
