db31d321bd1e01026289eca366f02c40939b739e
Maggie Yi <maggieyi@berkeley.edu>
2021-06-30 10:43:47 -0400

    profile settings

--- src/screens/main/ProfileModal.js
+++ src/screens/main/ProfileModal.js
@@ -1,7 +1,7 @@
-// EDIT THIS TO CHANGE USER INFO:COPIED RIGHT NOW!!!!
-
 import React, { Component }  from 'react';
 import { Text, SafeAreaView, View, TouchableOpacity, Image, Modal, TextInput, Alert, FlatList, Keyboard } from 'react-native';
+import { addBudget } from '../../app/actions/addBudget';
+import { updateRecurring } from '../../app/actions/updateRecurring';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withNavigation } from 'react-navigation';
@@ -17,19 +17,73 @@ export class ProfileModal extends Component {
         this.state ={
             modalVisible: true
         }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        this.setState({modalVisible: !this.state.modalVisible});
+        this.props.navigation.goBack();
     }
 
     handleComponentDidMount() {
         return (
-            <Modal visible={this.state.modalVisible} animationType="slide"> 
-                <SafeAreaView ref={this.wrapperRef} style={styles.modalContainer}>
+
+                <SafeAreaView style={styles.container}>
+
+                    {/* header */}
+                    <View style={{
+                        flexDirection: 'row', 
+                        justifyContent:'space-between',
+                        alignItems: 'baseline'
+                        }}>
+                        
+                        {/* go back */}
+                        <TouchableOpacity style={styles.backButton} onPress={() => this.props.navigation.navigate('Home')}>
+                            <Image 
+                            source={require('../../assets/Icons/back.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 20,
+                                height: 20,
+                                tintColor: '#7E9181',
+                            }}
+                            />
+                        </TouchableOpacity>
+
                 
-                    {/* Display name */}
-                    <Text style={styles.title2}>{this.props.currentUser.firstName} {this.props.currentUser.lastName}</Text>
-                    
+                    </View>
+
+                    <View style={{alignItems: 'center'}}>
+                        {/* profile picture */}
+                        <Image
+                            source={require('../../assets/Icons/profile.png')}
+                            style={{
+                                width: 80,
+                                height: 80
+                            }}
+                        />
+                        {/* Display name */}
+                        <Text style={styles.title2}>{this.props.currentUser.firstName} {this.props.currentUser.lastName}</Text>
+                        <Text style={styles.subtitle}>@{this.props.currentUser.username}</Text>
+
+                    </View>
+
+                    <Text></Text>
+
+                    <Text>{this.props.shortTerm}</Text>
+
+
+
+
+                    {/* Done Button */}
+                    <View style={{alignItems: 'center', justifyContent: 'center', marginBottom: 10}}>
+                        <TouchableOpacity onPress={() => this.handleClick()} style={styles.cancelButtonContainer}>
+                            <Text style={styles.cancelText}>Done</Text>
+                        </TouchableOpacity>
+                    </View>
 
                 </SafeAreaView>
-            </Modal>
         );
     }
 
@@ -40,12 +94,11 @@ export class ProfileModal extends Component {
     }
 };
 
-
-
 const mapStateToProps = (store) => ({
-    currentUser: store.user.currentUser
+    currentUser: store.user.currentUser,
+    longTerm: store.user.longTerm,
+    shortTerm: store.user.shortTerm
 });
 
-// const mapDispatchProps = (dispatch) => bindActionCreators({ addBudget, updateRecurring }, dispatch);
 
 export default connect(mapStateToProps, null)(withNavigation(ProfileModal));
