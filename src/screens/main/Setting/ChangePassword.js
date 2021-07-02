c6f283ed868908c2e915b8e9c98a070375b177e3
Maggie Yi <maggieyi@berkeley.edu>
2021-07-02 15:57:03 -0400

    change password functionality successful

--- src/screens/main/Setting/ChangePassword.js
+++ src/screens/main/Setting/ChangePassword.js
@@ -1,10 +1,46 @@
 import React, { useState } from 'react';
 import { Text, SafeAreaView, View, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import styles from '../../../styles/homeStyle';
+import firebase from 'firebase';
 
 const ChangePassword = ({ navigation }) => {
 
     const [error, setError] = useState('');
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [reNewPassword, setReNewPassword] = useState('');
+
+    // reauthenticate the user first if they are signed in for too long
+    const reauthenticate = (currentPassword) => {
+        var user = firebase.auth().currentUser;
+        var cred = firebase.auth.EmailAuthProvider.credential(
+            user.email, currentPassword);
+        return user.reauthenticateWithCredential(cred);
+    }
+
+    const changePassword = (currentPassword, newPassword) => {
+        reauthenticate(currentPassword).then(() => {
+            var user = firebase.auth().currentUser;
+            user.updatePassword(newPassword).then(() => {
+                setError("Password successfully updated!")
+            }).catch((error) => {
+                setError(error.message); 
+            });
+        }).catch((error) => {
+            setError(error.message); 
+        });
+    }
+
+
+    const handleSave = () => {
+        if (newPassword === reNewPassword) {
+            changePassword(oldPassword, newPassword);
+        } else {
+            setError("New passwords do not match.")
+        }
+        // navigation.goBack();
+    }
+
     const handleComponentDidMount = () => {
         return (
             <SafeAreaView style={styles.container2}>
@@ -22,8 +58,10 @@ const ChangePassword = ({ navigation }) => {
                         <Text style={styles.settingsText}>Current Password:</Text>
                         <TextInput
                             placeholder="Old password"
-                            onChangeText={(firstname) => setFirstname(firstname)}
+                            onChangeText={(value) => setOldPassword(value)}
                             style={styles.settingsInput}
+                            value={oldPassword}
+                            secureTextEntry={true}
                         />
                         <View style={{borderBottomColor: '#D7CEB2',borderBottomWidth: 1}}/>
                     </View>
@@ -33,19 +71,23 @@ const ChangePassword = ({ navigation }) => {
                         <Text style={styles.settingsText}>New Password:</Text>
                         <TextInput
                             placeholder="New password"
-                            onChangeText={(firstname) => setFirstname(firstname)}
+                            onChangeText={(value) => setNewPassword(value)}
                             style={styles.settingsInput}
+                            value={newPassword}
+                            secureTextEntry={true}
                         />
                         <View style={{borderBottomColor: '#D7CEB2',borderBottomWidth: 1}}/>
                     </View>
 
                     {/* main content */}
                     <View style={{margin: 5}}>
-                        <Text style={styles.settingsText}>Confire New Password:</Text>
+                        <Text style={styles.settingsText}>Confirm New Password:</Text>
                         <TextInput
                             placeholder="Re-enter new password"
-                            onChangeText={(firstname) => setFirstname(firstname)}
+                            onChangeText={(value) => setReNewPassword(value)}
                             style={styles.settingsInput}
+                            value={reNewPassword}
+                            secureTextEntry={true}
                         />
                         <View style={{borderBottomColor: '#D7CEB2',borderBottomWidth: 1}}/>
                     </View>
@@ -64,12 +106,12 @@ const ChangePassword = ({ navigation }) => {
                     {/* Buttons */}
                     <View style={{margin: 10, flexDirection: 'row', justifyContent: 'space-between', alignContent:'center'}}>
                     <TouchableOpacity style={styles.settingsButton}
-                        onPress={()=> navigation.goBack()}>
+                        onPress={() => handleSave()}>
                         <Text style={{fontSize: 16, fontWeight: 'bold', color: '#264653'}}>Save</Text> 
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.settingsButton}
                         onPress={()=> navigation.goBack()}>
-                        <Text style={{fontSize: 16, fontWeight: 'bold', color: '#264653'}}>Cancel</Text> 
+                        <Text style={{fontSize: 16, fontWeight: 'bold', color: '#264653'}}>Back</Text> 
                     </TouchableOpacity>
                 </View>
                     
