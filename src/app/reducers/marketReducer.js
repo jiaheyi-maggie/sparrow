a63d68ca52647d19dd62fa7ed419403a5c2be286
Maggie Yi <maggieyi@berkeley.edu>
2021-07-06 12:25:28 -0400

    crypto market api call

--- src/app/reducers/marketReducer.js
+++ src/app/reducers/marketReducer.js
@@ -14,10 +14,25 @@ const marketReducer = (state = initialState, action) => {
                 ...state,
                 loading: true
             }
-        case marketActions.GET_COIN_MARKET_SUCCESS:
+        case marketActions.GET_HOLDINGS_SUCCESS:
             return {
                 ...state,
                 myHoldings: action.payload.myHoldings
+            }
+        case marketActions.GET_HOLDINGS_FAILURE:
+            console.log(action.payload.error);
+            return {
+                ...state,
+                error: action.payload.error
+            }
+        case marketActions.GET_COIN_MARKET_BEGIN:
+            return {
+                ...state,
+            }
+        case marketActions.GET_COIN_MARKET_SUCCESS:
+            return {
+                ...state,
+                coins: action.payload.coins
             }
         case marketActions.GET_COIN_MARKET_FAILURE:
             return {
