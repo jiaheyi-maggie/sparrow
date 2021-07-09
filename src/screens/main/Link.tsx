4c48715e7fa75aa9884ea249bac4da3afb06128e
Maggie Yi <maggieyi@berkeley.edu>
2021-07-09 11:39:37 -0400

    get sandbox account data

--- src/screens/main/Link.tsx
+++ src/screens/main/Link.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, FunctionComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, SafeAreaView, View, ScrollView, FlatList, Pressable, TouchableOpacity, Image, NativeEventEmitter, NativeModules, Platform, Button } from 'react-native';
 import { COLORS, FONTS } from '../../constants/theme';
 import { PlaidLink } from '../../types/PlaidLink';
@@ -7,8 +7,53 @@ import styles from '../../styles/homeStyle';
 import OpenPlaidLink from '../../types/OpenPlaidLink';
 
 const Link = ({ navigation, link_token, client }) => {
+    const [publicToken, setPublicToken] = useState(null);
+    const [accessToken, setAccessToken] = useState(null);
+    const [institutionID, setInstitutionID] =  useState('ins_109508');
+	const [initialProducts, setInitialProducts] = useState(['auth', 'assets', 'balance', 'transactions']);
+
+    const getPublicTokenSandbox = async () => {
+		try {
+			const publicTokenResponse = await client.sandboxPublicTokenCreate(
+				institutionID,
+				initialProducts,
+			).catch((error) => {
+				console.log(error);
+			});
+			// const public_token = publicTokenResponse.public_token;
+			// setPublicToken(public_token);
+            console.log(publicTokenResponse);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+    const getAccessToken = async () => {
+		const exchangeTokenResponse = await client.exchangePublicToken(publicToken)
+			.catch((error) => {
+				console.log(error);
+			});
+        const accessToken = exchangeTokenResponse.access_token;
+        setAccessToken(accessToken);
+        console.log(exchangeTokenResponse);
+	};
+
+
+    const getAccounts = async () => {
+        client.getAccounts(accessToken, function (error, accountResponse) {
+            if (error != null) {
+                console.log(error);
+                return; 
+            }
+            console.log(accountResponse);
+        });
+    }
+
     
 	useEffect(() => {
+        getPublicTokenSandbox();
+        getAccessToken();
+        getAccounts();
 	}, [])
 
     const handleComponentDidMount = () => {
@@ -16,7 +61,7 @@ const Link = ({ navigation, link_token, client }) => {
 			<SafeAreaView style={styles.container2}>
 				<View style={styles.genericRow}>
 					<Text style={{color: COLORS.primary, ...FONTS.h2}}>Bank Accounts</Text>
-                    <PlaidLink token={link_token} />
+                    <PlaidLink token={link_token} client={client}/>
 				</View>
 				
 			</SafeAreaView>
--- src/types/PlaidLink.tsx
+++ src/types/PlaidLink.tsx
@@ -1,31 +1,87 @@
+import { Client } from 'plaid';
 import React, { useState, useCallback, FunctionComponent} from 'react';
 import { Button } from 'react-native';
-import { TouchableOpacity } from 'react-native';
-import { PlaidLinkPropTypes, usePlaidLink, PlaidLinkOptions, PlaidLinkOnSuccess, PlaidLinkOnExit, PlaidLinkOnEvent } from 'react-plaid-link';
+import { usePlaidLink, PlaidLinkOptions,  PlaidLinkError,
+    PlaidLinkOnSuccess, PlaidLinkOnSuccessMetadata,
+    PlaidLinkOnExit, PlaidLinkOnExitMetadata,
+    PlaidLinkOnEvent, PlaidLinkOnEventMetadata, PlaidLinkStableEvent } from 'react-plaid-link';
 
 interface Props {
     token: string;
+    client: Client;
 }
 
-export const PlaidLink: FunctionComponent<Props> = ({token}) => {
+export const PlaidLink: FunctionComponent<Props> = ({token, client}) => {
+    const [publicToken, setPublicToken] = useState(null);
+    const [accessToken, setAccessToken] = useState(null);
+    const [institutionID, setInstitutionID] =  useState('ins_109508');
+	const [initialProducts, setInitialProducts] = useState(['auth', 'assets', 'balance', 'transactions']);
+
+    const getPublicTokenSandbox = async () => {
+		try {
+			const publicTokenResponse = await client.sandboxPublicTokenCreate(
+				institutionID,
+				initialProducts,
+			).catch((error) => {
+				console.log(error);
+			});
+			// const public_token = publicTokenResponse.public_token;
+			// setPublicToken(public_token);
+            console.log(publicTokenResponse);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+    const getAccessToken = async () => {
+		const exchangeTokenResponse = await client.exchangePublicToken(publicToken)
+			.catch((error) => {
+				console.log(error);
+			});
+        const accessToken = exchangeTokenResponse.access_token;
+        setAccessToken(accessToken);
+        console.log(exchangeTokenResponse);
+	};
+
     const onSuccess = useCallback<PlaidLinkOnSuccess>(
-        (public_token, metadata) => {
+        (public_token: string, metadata: PlaidLinkOnSuccessMetadata) => {
             // send public_token to server
-            console.log(public_token, 'success');
+            getPublicTokenSandbox()
+            // fetch('/api/set_access_token', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({
+            //         public_token: public_token,
+            //         metadata: metadata,
+            //     })
+            // })
+            // .then((response) => {
+            //     console.log('success', response);
+            // })
+            // .catch((error) => {
+            //     console.log(error);
+            // })
         },[]
     );
 
     const onExit = useCallback<PlaidLinkOnExit>(
-        (error, metadata) => {
+        (error: PlaidLinkError, metadata: PlaidLinkOnExitMetadata) => {
             // send public_token to server
-            console.log(error, 'exit');
+            console.log('exit', metadata);
+
+            if (error != null && error.error_code === 'INVALID_LINK_TOKEN') {
+                // TODO: generate new link token
+                console.log(error);
+            }
         },[]
     );
 
     const onEvent = useCallback<PlaidLinkOnEvent>(
-        (eventName, metadata) => {
+        (eventName: PlaidLinkStableEvent | string , metadata: PlaidLinkOnEventMetadata) => {
             // send public_token to server
-            console.log(eventName, 'event');
+            console.log(eventName, metadata);
         },[]
     );
 
