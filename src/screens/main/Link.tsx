0cf49604d5f0c1276b09619482069d29b1188f2b
Maggie Yi <maggieyi@berkeley.edu>
2021-07-13 13:16:20 -0400

    embedded webview

--- src/screens/main/Link.tsx
+++ src/screens/main/Link.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, FunctionComponent } from 'react';
-import { Text, SafeAreaView, View, ScrollView, FlatList, Pressable, TouchableOpacity, Image, NativeEventEmitter, NativeModules, Platform, Button } from 'react-native';
+import { Text, SafeAreaView, View, FlatList, Pressable, TouchableOpacity, Image, NativeEventEmitter, NativeModules, Platform, Button } from 'react-native';
 import { COLORS, FONTS } from '../../constants/theme';
 import { PlaidLink } from '../../types/PlaidLink';
-// import { PlaidLink } from 'react-native-plaid-link-sdk';
 import { connect } from 'react-redux';
 import styles from '../../styles/homeStyle';
 import BankBalanceInfo from '../../components/main/BankBalanceInfo';
@@ -132,7 +131,11 @@ const Link = ({ navigation, link_token, client }) => {
 				<View style={[styles.genericRow, {marginBottom: 10}]}>
 					<Text style={{color: COLORS.primary, ...FONTS.h2}}>Bank Accounts</Text>
                     {/* TODO: PlaidLink leads to OAuth */}
-                    <PlaidLink token={link_token} client={client}/>
+                    {/* <PlaidLink token={link_token} client={client}/> */}
+                    <Button
+                        title="Add"
+                        onPress={() => navigation.navigate("WebPlaidLink")}
+                    />
 				</View>
 
                 <View style={styles.genericRow}> 
--- src/types/WebPlaidLink.tsx
+++ src/types/WebPlaidLink.tsx
@@ -1,8 +1,76 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent, useCallback, useEffect } from 'react';
 import WebView from 'react-native-webview';
+import { usePlaidLink, PlaidLinkOptions,
+    PlaidLinkOnSuccess, PlaidLinkOnSuccessMetadata, 
+    PlaidLinkOnEventMetadata, PlaidLinkStableEvent, PlaidLinkOnEvent,
+    PlaidLinkOnExitMetadata, PlaidLinkOnExit, PlaidLinkError } from 'react-plaid-link';
+import { Client } from 'plaid';
+import { connect } from 'react-redux';
+import { COLORS, FONTS } from '../constants/theme';
 
-export default class WebPlaidLink extends Component {
-    render() {
-        return <WebView source={{uri: 'https://expo.io'}} style={{marginTop: 20}}/>
-    }
+interface Props {
+    token: string;
+    client: Client;
 }
+
+const WebPlaidLink: FunctionComponent<Props>  = ({ token, client }) => {
+
+    const onSuccess = useCallback<PlaidLinkOnSuccess>(
+        (public_token: string, metadata: PlaidLinkOnSuccessMetadata) => {
+            console.log(public_token);
+        },[]
+    );
+    
+        const onExit = useCallback<PlaidLinkOnExit>(
+            (error: PlaidLinkError, metadata: PlaidLinkOnExitMetadata) => {
+                // send public_token to server
+                console.log('exit', metadata);
+    
+                if (error != null && error.error_code === 'INVALID_LINK_TOKEN') {
+                    const response = client
+                    .createLinkToken({
+                        user: {
+                            client_user_id: "1234",
+                        },
+                        client_name: "Sparrow",
+                        products: ["auth", "transactions"],
+                        country_codes: ["US"],
+                        language: "en",
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+                    console.log(response);
+                }
+            },[]
+        );
+    
+        const onEvent = useCallback<PlaidLinkOnEvent>(
+            (eventName: PlaidLinkStableEvent | string , metadata: PlaidLinkOnEventMetadata) => {
+                console.log(eventName, metadata);
+            },[]
+        );
+    
+        const config: PlaidLinkOptions = {
+            token,
+            onSuccess,
+            onExit,
+            onEvent,
+        };
+
+        const { open, exit, ready } = usePlaidLink(config);
+
+        useEffect(() => {
+            open()
+        }, []);
+    
+
+    return (
+        <WebView source={{uri: 'https://expo.io'}} style={{marginTop:50}}/>
+    );
+}
+
+const mapStateToProps = (store) => ({
+    client: store.plaidReducer.client,
+})
+export default connect(mapStateToProps, null)(WebPlaidLink);
