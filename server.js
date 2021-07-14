42076b2542c416a77b353f2164bd3a3c7bc8fc5a
Maggie Yi <maggieyi@berkeley.edu>
2021-07-14 12:39:07 -0400

    getting user information

--- server.js
+++ server.js
@@ -3,20 +3,11 @@ const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-
+const port = 19002;
 const app = express();
 dotenv.config(); 
-// app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-let ACCESS_TOKEN = null;
-let PUBLIC_TOKEN = null;
-let ITEM_ID = null;
-let LINK_TOKEN = null;
-
-const port = 19002;
 
 const plaid = require('plaid');
 
@@ -27,6 +18,21 @@ const client = new plaid.Client({
     env: plaid.environments.sandbox,
 });
 
+var ACCESS_TOKEN;
+var PUBLIC_TOKEN;
+var ITEM_ID;
+var LINK_TOKEN;
+
+function handleError(error) {
+    console.log(error);
+}
+
+const formatError = (error) => {
+    return {
+      error: { ...error.data, status_code: error.status },
+    };
+};
+
 // front page: link
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'src/types/plaid-link.html'));
@@ -36,11 +42,6 @@ app.get('/', (request, response) => {
 app.get('/client', (req, res) => {
     res.send(client);
 })
-
-function handleError(error) {
-    console.log(error);
-}
-
 
 // create link token
 app.post('/create_link_token', async (req, res) => {
@@ -63,23 +64,6 @@ app.post('/create_link_token', async (req, res) => {
     const linkToken = response.link_token;
     LINK_TOKEN = linkToken;
     res.json(linkToken);
-})
-
-// do not need to regenerate link token again
-app.get('/create_link_token', async (req, res) => {
-    const configs = {
-        user: {
-            client_user_id: "1234",
-        },
-        client_name: "Sparrow",
-        products: ["auth", "transactions"],
-        country_codes: ["US"],
-        language: "en",
-    }
-
-    const response = await client.createLinkToken(configs);
-    const link_token = response.link_token;
-    res.send(link_token);
 })
 
 // exchange public token for access token
@@ -105,10 +89,105 @@ app.post('/plaid_token_exchange', async (request, response) => {
         handleError(error);
         return response.json(formatError(error.response));
     }
-
     // const { access_token } = await client.exchangePublicToken(publicToken).catch(handleError);
     // const { accounts, item } = await client.getAccounts(access_token).catch(handleError);
 })
+
+// app.get('/create_link_token', async (req, res) => {
+//     const configs = {
+//         user: {
+//             client_user_id: "1234",
+//         },
+//         client_name: "Sparrow",
+//         products: ["auth", "transactions"],
+//         country_codes: ["US"],
+//         language: "en",
+//     }
+
+//     const response = await client.createLinkToken(configs);
+//     const link_token = response.link_token;
+//     // LINK_TOKEN=link_token;
+//     // console.log(LINK_TOKEN);
+//     res.send(link_token);
+// })
+
+// investment transavtions for an Item
+app.get('/transactions', async (req, res) => {
+    // pull transactions for item for the last 30 days
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth();
+    var day = now.getDate();
+    var dayStart = day - 30; 
+
+    const endDate = `${year}-${month}-${day}`;
+    const startDate = `${year}-${month}-${dayStart}`;
+
+    const configs = {
+        access_token: ACCESS_TOKEN,
+        start_date: startDate,
+        end_date: endDate,
+    }
+
+    const response = await client.getInvestmentTransactions(configs)
+    .catch(handleError);
+
+    res.json(response);
+})
+
+// Retrieve real-time Balances for each of an Item's accounts
+app.get('/balance', async function (req, res) {
+    try {
+      const balanceResponse = await client.getBalance(ACCESS_TOKEN);
+      res.json(balanceResponse.data);
+    } catch (error) {
+      return res.json(formatError(error.response));
+    }
+  });
+  
+// Retrieve Holdings for an Item
+app.get('/holdings', async function (req, res) {
+    try {
+        const holdingsResponse = await client.getHoldings(ACCESS_TOKEN);
+        res.json({ error: null, holdings: holdingsResponse.data });
+    } catch (error) {
+        return res.json(formatError(error.response));
+    }
+});
+
+// Retrieve information about an Item
+app.get('/item', async function (req, res, next) {
+    try {
+        // Pull the Item - this includes information about available products,
+        // billed products, webhook information, and more.
+        const itemResponse = await client.getItem(ACCESS_TOKEN);
+        // Also pull information about the institution
+        const configs = {
+            institution_id: itemResponse.data.item.institution_id,
+            country_codes: ['US'],
+        };
+        const instResponse = await client.getInstitutionById(configs);
+        res.json({
+            item: itemResponse.data.item,
+            institution: instResponse.data.institution,
+        });
+    } catch (error) {
+        handleError(error);
+        return response.json(formatError(error.response));
+    }
+});
+
+// Retrieve an Item's accounts
+app.get('/accounts', async function (req, res, next) {
+    try {
+      const accountsResponse = await client.getAccounts(ACCESS_TOKEN);
+      res.json(accountsResponse.data);
+    } catch (error) {
+        handleError(error);
+      return res.json(formatError(error.response));
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
