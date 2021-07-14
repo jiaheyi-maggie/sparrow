a0b6d0dc19ff7ae6af71dd9f57c78da54d05febf
Maggie Yi <maggieyi@berkeley.edu>
2021-07-14 12:01:30 -0400

    post token exchange logic to server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // nodeJS server
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
 const path = require('path');
-const port = 19002;
+
 
 const app = express();
 dotenv.config(); 
@@ -11,18 +11,28 @@ dotenv.config();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+let ACCESS_TOKEN = null;
+let PUBLIC_TOKEN = null;
+let ITEM_ID = null;
+let LINK_TOKEN = null;
+
+const port = 19002;
+
 const plaid = require('plaid');
 
+// create a plaid client: this app
 const client = new plaid.Client({
     clientID: process.env.PLAID_CLIENT_ID,
     secret: process.env.PLAID_SECRET,
     env: plaid.environments.sandbox,
 });
 
+// front page: link
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'src/types/plaid-link.html'));
 })
 
+// client
 app.get('/client', (req, res) => {
     res.send(client);
 })
@@ -31,21 +41,10 @@ function handleError(error) {
     console.log(error);
 }
 
-app.post('/plaid_token_exchange', async (request, response) => {
-    const { publicToken } = request.body;
 
-    const { access_token } = await client.exchangePublicToken(publicToken).catch(handleError);
-
-    const { accounts, item } = await client.getAccounts(access_token).catch(handleError);
-
-    console.log({
-        accounts,
-        item
-    })
-})
-
-app.get('/create_link_token', async (req, res) => {
-    const response = await client.createLinkToken({
+// create link token
+app.post('/create_link_token', async (req, res) => {
+    const configs = {
         user: {
             client_user_id: "1234",
         },
@@ -53,10 +52,62 @@ app.get('/create_link_token', async (req, res) => {
         products: ["auth", "transactions"],
         country_codes: ["US"],
         language: "en",
-    }).catch(handleError);
+    }
 
+    // for oauth
+    if (process.env.PLAID_REDIRECT_URI != '') {
+        configs.redirect_uri = PLAID_REDIRECT_URI;
+    }
+
+    const response = await client.createLinkToken(configs);
+    const linkToken = response.link_token;
+    LINK_TOKEN = linkToken;
+    res.json(linkToken);
+})
+
+// do not need to regenerate link token again
+app.get('/create_link_token', async (req, res) => {
+    const configs = {
+        user: {
+            client_user_id: "1234",
+        },
+        client_name: "Sparrow",
+        products: ["auth", "transactions"],
+        country_codes: ["US"],
+        language: "en",
+    }
+
+    const response = await client.createLinkToken(configs);
     const link_token = response.link_token;
     res.send(link_token);
+})
+
+// exchange public token for access token
+app.post('/plaid_token_exchange', async (request, response) => {
+    const { publicToken } = request.body.public_token;
+    PUBLIC_TOKEN = publicToken;
+
+    try {
+        // const tokenResponse = await client.exchangePublicToken({public_token: PUBLIC_TOKEN});
+        const tokenResponse = await client.exchangePublicToken(publicToken);
+        const { access_token } = tokenResponse.data.access_token;
+        const { item_id } = tokenResponse.data.item_id;
+
+        ACCESS_TOKEN = access_token;
+        ITEM_ID = item_id;
+
+        response.json({
+            access_token: access_token,
+            item_id: item_id,
+            error: null,
+        });
+    } catch(error) {
+        handleError(error);
+        return response.json(formatError(error.response));
+    }
+
+    // const { access_token } = await client.exchangePublicToken(publicToken).catch(handleError);
+    // const { accounts, item } = await client.getAccounts(access_token).catch(handleError);
 })
 
 app.listen(port, () => {
