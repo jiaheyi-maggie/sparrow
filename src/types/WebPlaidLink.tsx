87fd818af576f50ce33a413d43504d2065a5d862
Maggie Yi <maggieyi@berkeley.edu>
2021-07-15 18:06:49 -0400

    webpage navigation

--- src/types/WebPlaidLink.tsx
+++ src/types/WebPlaidLink.tsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { View, Modal, TouchableOpacity, Text, Image } from 'react-native';
 import styles from '../styles/homeStyle';
 import WebView from 'react-native-webview';
-import { Client } from 'plaid';
+import { COLORS, FONTS } from '../constants/theme';
 import { connect } from 'react-redux';
 
 // interface Props {
@@ -11,47 +11,34 @@ import { connect } from 'react-redux';
 // }
 
 const WebPlaidLink = ({ navigation }) => {
-    const [modalVisible, setModalVisible] = useState(false);
-
-    const handleOpen = () => {
-        return (
-            <WebView source={{uri: 'http://192.168.1.20:19002'}} style={{flex:1, marginTop: 50}}/>
-            
-        );
-    }
 
     const handleRendering = () => {
         return (
-            <View style={{marginTop: 50}}>
-                <Modal
-                animationType="fade"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => setModalVisible(!modalVisible)}
-                >
-                    {/* View for the list of time periods */}
-                    <View style={styles.centeredView}>
-                        <View style={styles.modalView}>
-                            <Text style={styles.listText2}>Select a time period</Text>
-                        </View>
-                    </View>
-                </Modal>
-
-                <TouchableOpacity style={[styles.button, styles.buttonOpen]} onPress={() => handleOpen()}>
-                    <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-between'}}>
+            <View style={[styles.container3, {flex: 1}]}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <View style={{flexDirection: 'row', alignItems:'center'}}>
                         <Image 
-                        source={require('../assets/Icons/down-arrow.png')} 
-                        resizeMode='contain'
-                        style={{marginTop: 5, width: 20, height: 20, tintColor: '#fff'}}
+                            source={require('../assets/Icons/back.png')}
+                            style={{
+                                width: 13,
+                                height: 13,
+                                tintColor: COLORS.primary,
+                                marginRight: 5
+                            }}
                         />
+                        <Text style={{...FONTS.h4, color: COLORS.primary}}>Back</Text>
                     </View>
+                    
                 </TouchableOpacity>
+                <WebView source={{uri: 'http://192.168.1.20:19002'}} 
+                    style={{flex:1,  height: 200}}
+                />
             </View>
         );
     }
+
     return (
-        <WebView source={{uri: 'http://192.168.1.20:19002'}} style={{flex:1, marginTop: 50}}/>
-        // handleRendering()
+        handleRendering()
         
     );
 }
