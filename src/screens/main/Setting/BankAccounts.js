dcbff61bbc3241ee2ce9477fa0d3f7cc7824f3a3
Maggie Yi <maggieyi@berkeley.edu>
2021-07-30 15:40:51 -0400

    bank account settings population

--- src/screens/main/Setting/BankAccounts.js
+++ src/screens/main/Setting/BankAccounts.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import { Text, SafeAreaView, View, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, SafeAreaView, View, ScrollView, TouchableOpacity, FlatList } from 'react-native';
 import styles from '../../../styles/homeStyle';
+import { connect } from 'react-redux';
+import store from '../../../app/store';
 
-const BankAccounts = ({ navigation }) => {
+const BankAccounts = ({ navigation, bank_accounts }) => {
+
+    const [accounts, setAccounts] = useState(null);
+
+    useEffect(() => {
+        setAccounts(store.getState().plaidReducer.bank_accounts);
+        // console.log("accounts");
+        // console.log(accounts);
+    }, [])
+
     const handleComponentDidMount = () => {
         return (
             <SafeAreaView style={styles.container2}>
-                <ScrollView>
+                {/* header */}
+                <View style={{alignItems: 'center'}}>
+                    {/* Display name */}
+                    <Text style={styles.title}>Bank Accounts</Text>   
+                </View>
 
-                    {/* header */}
-                    <View style={{alignItems: 'center'}}>
-                        {/* Display name */}
-                        <Text style={styles.title}>Bank Accounts</Text>   
-                    </View>
+                {/* main content */}
+                <FlatList
+                    data={accounts}
+                    renderItem={({item})=> {
+                        return (
+                            <View style={[styles.genericRow ,{backgroundColor:'aliceblue', margin: 10, padding: 8, alignItems:'center'}]}>
+                                <Text style={{color: 'black'}}>{item.name}</Text>
+                                <TouchableOpacity style={[styles.buttonOpen, {padding: 5, borderRadius: 10}]}>
+                                    <Text>More</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )
+                    }}
+                    keyExtractor={item => item._id}
+                />
 
-                    {/* main content */}
-
-                    {/* Buttons */}
-                    <View style={{margin: 10, flexDirection: 'row', justifyContent: 'space-between', alignContent:'center'}}>
+                {/* Buttons */}
+                <View style={{margin: 10, flexDirection: 'row', justifyContent: 'space-between', alignContent:'center'}}>
                     <TouchableOpacity style={styles.settingsButton}
                         onPress={()=> navigation.goBack()}>
                         <Text style={{fontSize: 16, fontWeight: 'bold', color: '#264653'}}>Done</Text> 
@@ -28,7 +51,6 @@ const BankAccounts = ({ navigation }) => {
                     </TouchableOpacity>
                 </View>
                     
-                </ScrollView>
             </SafeAreaView>
         );
     }
@@ -39,4 +61,8 @@ const BankAccounts = ({ navigation }) => {
 
 };
 
-export default BankAccounts;
+const mapStateToProps = (store) => ({
+    bank_accounts: store.plaidReducer.bank_accounts,
+})
+
+export default connect(mapStateToProps, null)(BankAccounts);
